bf7bbe131c9bf3ee790a5f976250e88549c48fdb
Aleksei Ogoltcov <aaogoltcov@mail.ru>
2020-06-12 11:28:22 +0300

    Mistakes resolved

--- task.js
+++ task.js
@@ -1,39 +1,34 @@
 'use strict';
 
 // Task 1
-function getEqRoot(a, b, c) {
-    return (Math.pow(b, 2) - (4 * a * c));
+function getEquationDiscriminant(a, b, c) {
+    return (Math.pow(b, 2) - (4 * a * c)); // вычисляем дискриминант
 }
 
-function rootEq(a, b, c) {
-    let root = getEqRoot(a, b, c);
+function getResult(a, b, c) {
+    let root = getEquationDiscriminant(a, b, c);
     if (root > 0) {
-        return [`${(-b + Math.sqrt(root)) / (2 * a)}`, `${(-b - Math.sqrt(root)) / (2 * a)}`];
+        return [(-b + Math.sqrt(root)) / (2 * a), (-b - Math.sqrt(root)) / (2 * a)]; // вычисляем корни
     } else if (root === 0) {
-        return [`${(-b + Math.sqrt(root)) / (2 * a)}`];
+        return [(-b + Math.sqrt(root)) / (2 * a)]; // вычисляем корень
     } else {
-        return [];
+        return []; // если дискриминант меньше нуля - возвращаем пустой массив
     }
 }
 
-function getResult(a,b,c){
-    return rootEq(a, b, c);
-}
-
-
 // Task 2
-function getAverageScore(scoreSum, scoreNumbers) {
-    return Math.round((scoreSum / scoreNumbers), 2);
-}
-
 function getScoreSum(array, arrayLength, iteration=0, scoreSum=0, scoreNumbers=0) {
     for (iteration = iteration; iteration < arrayLength; iteration += 1) {
         if (array[iteration] && scoreNumbers < 5) {
             scoreSum += array[iteration];
             scoreNumbers += 1;
         }
+        if (iteration > 4 ){
+            console.log(`Количество значение в массиве больше 5, обрезали: ${array.slice(0, 5)}`);
+            break;
+            }
     }
-    return [scoreSum, scoreNumbers, iteration];
+    return [scoreSum, scoreNumbers];
 }
 
 function getAverageMark(marks){
@@ -42,17 +37,13 @@ function getAverageMark(marks){
         return 0;
     } else {
         let scoreSum = getScoreSum(marks, arrayLength)
-        if (scoreSum[2] > 5 ){
-            console.log(`Количество значение в массиве больше 5, обрезали: ${marks.slice(0, 5)}`);
-        }
-        return getAverageScore(scoreSum[0], scoreSum[1]);
+        return Math.round((scoreSum[0] / scoreSum[1]), 2);
     }
 }
 
 // Task 3
 function askDrink(name,dateOfBirthday){
-    let response = ((new Date().getFullYear() - dateOfBirthday.getFullYear()) >= 18) ?
-                    `Не желаете ли олд-фэшн, ${name}?` :
-                    `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
-    return response;
+    return ((new Date().getFullYear() - dateOfBirthday.getFullYear()) >= 18) ?
+                               `Не желаете ли олд-фэшн, ${name}?` :
+                               `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
 }
